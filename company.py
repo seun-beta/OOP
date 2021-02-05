7a7e332a8000cd330a140b0f7386ed89dd094fbe
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-05 14:41:44 +0000

    renamed .py file according to PEP 8
    Signed-off-by: GitHub <noreply@github.com>

---
+++ company.py
@@ -0,0 +1,43 @@
+class Addax:
+    office = 'Lagos Office'
+    ext = '+234'
+    num_of_emps = 0
+
+    def __init__(self, first, last, age,sex, phone) :
+        self.first = first
+        self.last = last
+        self.phone = phone 
+        self.age = self.fullname()+ "'s age is: " + str(age)
+        
+        Employee.num_of_emps +=1  #For counting the number of employees added to the class.
+
+        if sex == 'M':
+            self.sex = 'Male'
+        elif sex == 'F':
+            self.sex = 'Female'
+        else:
+            self.sex = "Input a valid sex either 'M' or 'F'"
+            
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def pnumber(self):
+        self.pnumber = Addax.ext + str(self.phone)
+        return self.pnumber
+    def location(self):
+        self.location = Addax.office
+        return self.location
+        
+
+addax_1 = Addax('Seun','Adegoke','21','M',812)
+
+print (addax_1.sex)
+print (addax_1.pnumber())
+print (addax_1.location())
+
+addax_2 = Addax('Jimoh','Muhammed',23,'M','867')
+print (addax_2.age)
+print (addax_1.age)
+
+
+            
