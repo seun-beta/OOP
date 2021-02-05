49d7b929d88fbf272cb21d9006f55d73289c6dcf
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-05 14:51:05 +0000

    create shoe.py

---
+++ shoe.py
@@ -0,0 +1,31 @@
+class Shoe:
+    # Class variables 'brand' & 'group'
+    brand = 'Nike'
+    group = 'Men'
+
+    # Instance variables 'name' & 'size' from the constructor method 
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+
+    # Instance variable 'outsole' from the method 
+    def outsole_material(self, outsole):
+        return ("The outsole material is " + outsole)
+
+def main():
+    shoe_1 = Shoe('Air Max', 42)
+    print(shoe_1.name)
+    print(shoe_1.size)
+    print(shoe_1.brand)
+    shoe_1.outsole_material('Natural Rubber')
+    print()
+    shoe_2 = Shoe('React', 56)
+    print(shoe_2.brand)
+    print(shoe_2.name)
+    print(shoe_2.size)
+    shoe_2.outsole_material('Polyuretane')
+    
+
+if __name__ =='__main__':
+    main()
+
