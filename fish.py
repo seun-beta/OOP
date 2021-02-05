8a288e94e0ccda180ca18d70b627951667305e43
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-05 15:05:45 +0000

    upload code

---
+++ fish.py
@@ -0,0 +1,34 @@
+class Fish:
+    def __init__(self, first_name, last_name='Fish', skeleton='bone', eyelids=False):
+        print("The object has been instantiated")
+        self.first_name = first_name
+        self.last_name = last_name
+        self.skeleton = skeleton
+        self.eyelids = eyelids
+    def swim(self):
+        print("The fish is swimming.")
+
+    def swim_backwards(self):
+        print("The fish can swim backwards.")        
+
+
+class Trout(Fish):
+    pass
+
+terry = Trout('Terry')
+print(terry.skeleton)
+print(terry.eyelids)
+print(terry.last_name)
+terry.swim()
+terry.swim_backwards()
+
+
+class Clownfish(Fish):
+
+    def live_with_anemone(self):
+        print("The clownfish coexists with sea anemone")
+
+casey = Clownfish('Casey')
+casey.live_with_anemone()
+print(casey.eyelids)
+print(casey.first_name)
