1cf2d238e96d6d43628d044640b12f8887778a1a
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-07 21:21:10 +0000

    fixed error

--- order.py
+++ order.py
@@ -5,12 +5,14 @@ class Order:
     def __init__(self, product_name, quantity, product = 'Shoe'):
         self.product_name = product_name
         self.quantity = quantity
+        self.product = product
         Order.number_of_orders += 1
 
     def product_description(self):
-        return product + ': ' + str(self.quantity) 
+        return self.product + ': ' + str(self.quantity) 
 
 
 order_1 = Order('Nike Air Max', 10)
 print(order_1.number_of_orders)
 print(order_1.quantity)
+print(order_1.product_description())
